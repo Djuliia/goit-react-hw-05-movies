dad80ea128483bd046ae61e2dbf8513d4e0fe320
Yuliia Chaika <124448012+Djuliia@users.noreply.github.com>
2023-10-06 20:52:43 +0300

    Update index.js
    added '/' into basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-react-hw-05-movies">
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
       <Toaster position="top-right" />
     </BrowserRouter>
